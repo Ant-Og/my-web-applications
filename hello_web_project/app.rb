f5a28e53ea19bfd12d2e07ad11761d72a5a26565
Ant-Og <hackerman.ant@gmail.com>
2023-02-28 15:20:29 +0000

    built GET and POST routes

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -1,20 +1,22 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class Application < Sinatra::Base
-  
-  get '/' do
 
-    return 'Hello!'
+  configure :development do
+    register Sinatra::Reloader
   end
-    
-  get "/hello" do
+
+  get '/hello' do
     name = params[:name]
+
     return "Hello #{name}"
   end
 
-  post "/submit" do
-    name =params[:name]
-    message =params[:message]
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+
     return "Thanks #{name}, you sent this message: #{message}"
-  end 
+  end
 end
