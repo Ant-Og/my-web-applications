eee4b4fc0920f997113e9ecf03ab518aa813eccd
Ant-Og <hackerman.ant@gmail.com>
2023-03-04 18:13:42 +0000

    get /names route added, rspec test passed

--- hello_web_project/spec/integration/app_spec.rb
+++ hello_web_project/spec/integration/app_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 require 'rack/test'
-require_relative    '../../app'
+require_relative '../../app'
 
 describe Application do
+
   include Rack::Test::Methods
 
   let(:app) { Application.new }
 
-  context 'GET /' do
+  context 'GET /hello' do
     it 'returns 200 ok' do
-      response = get('/')
+      response = get('/hello?name=Anthony')
       
       expect(response.status).to eq(200)
-      expect(response.body).to eq('Hello!')
+      expect(response.body).to eq('Hello Anthony')
     end
   end 
   
@@ -20,7 +21,26 @@ describe Application do
     it 'returns Hello name and message' do
       response = post('/submit', name: 'Anthony', message: 'How are you?')
       
+      expect(response.status).to eq(200)
       expect(response.body).to eq('Thanks Anthony, you sent this message: How are you?')
     end
   end
+  
+  context 'GET /names' do
+    it 'returns a string of names' do
+      response = get('/names?names=Anthony, Tony, Tone')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('Anthony, Tony, Tone')
+    end
+  end
+
+#   context 'POST /sort-names' do
+#     it 'returns a string of names sorted in alphabetical order' do
+#       response = post('/sort-names', names: 'Will, Anthony, JC, Henry')
+      
+#       expect(response.status).to eq(200)
+#       expect(response.body).to eq('Anthony Henry, JC, Will')
+#     end
+#   end
 end
